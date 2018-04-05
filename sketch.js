b2bef5d374e7487507100700467577811af24dae
Dylan Wansbrough <33749606+Dylan-Wansbrough@users.noreply.github.com>
2018-04-06 11:51:12 +1200

    setting up JS

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  var canvas = createCanvas(windowWidth, windowHeight/1.05);
+ 
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  canvas.parent('sketch-holder');
+
+  background(179, 182, 183);
+}
+
+function draw() {
+
+}
