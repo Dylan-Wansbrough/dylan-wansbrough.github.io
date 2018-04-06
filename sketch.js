6bd2eec1db91628743d753f709feab901ac1e98d
Dylan Wansbrough <dylan.wansbrough@hotmail.com>
2018-04-06 20:42:56 +1200

    colour changes to show live

--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ var ellipseY = []; //Arry of Y values for the bubbles
 var velocityX = [-1,1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1,-1,1]; //X value velocity
 var velocityY = [-0.5,-0.5,0.5,-0.5,0.5,-0.5,0.5,0.5,0.5,-0.5,0.5,0.5,-0.5,0.5,-0.5,0.5,0.5,-0.5,0.5]; //Y value velocity
 var x, y, vx, vy, xdistance, ydistance; //various values
-var counter = 50;
+var counter = 250;
+var start = false;
 
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight/1.05);
@@ -11,19 +12,19 @@ function setup() {
   // Move the canvas so it’s inside our <div id="sketch-holder">.
   canvas.parent('sketch-holder');
 
-  background(179, 182, 183);
-  create();
+  background(5, 42, 73);
 }
 
 function draw() {
-  background(179, 182, 183);
+  if(!start){create();}
+  background(5, 42, 73);
   var i = 0;
-  while(i < ellipseX.length){
+  while(i < counter){
     x = ellipseX[i];
     y = ellipseY[i];
     fill(179, 182, 183);
     // stroke(179, 182, 183);
-    stroke(255);
+    stroke(75, 208, 244);
     ellipse(x,y, 5,5);
 
 
@@ -40,25 +41,26 @@ function draw() {
       vy = vy * (-1);
     }
 
-    velocityX[i] = vx;
-    velocityY[i] = vy;
-    ellipseX[i] = x;
-    ellipseY[i] = y;
-
     var c = 0;
-    while(c < ellipseX.length){
+    while(c < counter){
       if(c != i){
         x2 = ellipseX[c];
         y2 = ellipseY[c];
         xdistance = x - x2;
         ydistance = y - y2;
         if(xdistance < 150 && xdistance > -150 && ydistance < 150 && ydistance > -150){
-        stroke(255);
+        stroke(75, 208, 244);
         line(x, y, x2, y2);
         }
       }
-      c++;
+      c += 1;
     }
+
+    velocityX[i] = vx;
+    velocityY[i] = vy;
+    ellipseX[i] = x;
+    ellipseY[i] = y;
+
 
     i += 1;
   }
@@ -66,6 +68,7 @@ function draw() {
 stroke(0);
 fill(0);
 textSize(200);
+// textFont(metaBold, 44);
 text("Dylan Wansrough", windowWidth/10, windowHeight/2);
 }
 
@@ -74,14 +77,9 @@ text("Dylan Wansrough", windowWidth/10, windowHeight/2);
 function create(){
     for (index = 0; index < counter; index += 1) {
       var random = Math.floor((Math.random() * windowWidth) + 1);
-      var random2 = Math.floor((Math.random() * windowHeight) + 1);
+      var random2 = Math.floor((Math.random() * windowHeight/1.05) + 1);
         ellipseX[index] = random;
         ellipseY[index] = random2;
-        /*
-        // alternative:
-        myArray.push([]);
-        // one-liner
-        for (index = 0; index < counter; index += 1) myArray.push([]);
-        */
     }
+    start = true;
 }
